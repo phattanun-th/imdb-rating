8c39a0f62b84569d99c92c53d2ae4dcd16d38cfd
phattanun-th <84891534+phattanun-th@users.noreply.github.com>
2021-09-29 18:59:08 +0700

    git commit -m "initial version"

---
+++ main.py
@@ -0,0 +1,9 @@
+# ===================== Libraries used =====================
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import  train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+
+
