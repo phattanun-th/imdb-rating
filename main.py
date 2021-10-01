a42b6678bb9adceff0b78832ca71420bcce52bbf
phattanun-th <84891534+phattanun-th@users.noreply.github.com>
2021-10-01 23:53:23 +0700

    update pre-processing

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import  train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
-os.chdir('D:/project/git-repo/imdb-rating/')
+os.chdir('M:/project/git-repo/imdb-rating/')
 df = pd.read_csv('./movies.csv')
 
 # ==========================================
@@ -31,7 +31,15 @@ df['year'] = df['year'].astype('category')
 # Should we impute missing values?
 sns.set_theme(style="whitegrid")
 sns.boxplot(x=df['budget'])
-sns.displot(df, x="budget")
+# sns.displot(df, x="budget", binwidth = 10)
 # sns.displot(df, x="budget",binwidth=50, hue="company")
+df['rating'].value_counts() 
+df['company'].value_counts() # 2385 companies
+df.select_dtypes(['float64']) # select specific types
+df.select_dtypes(['category'])
 
-
+# To do
+# drop NA of released, socre, vote, writer, star, country, company, runtime
+# decide whether shoud we delete budget columns or not
+# check outlier of numerical values
+# impute rating colums (maybe based on mode)
